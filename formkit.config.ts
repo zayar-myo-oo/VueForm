344c8f0476fafee275f8d40859e15ec665dd0e4f
zayarmyooo19 <hanzohasashi880@gmail.com>
2024-02-09 00:33:44 +0630

    first setup

---
+++ formkit.config.ts
@@ -0,0 +1,9 @@
+import { genesisIcons } from "@formkit/icons"
+import { rootClasses } from "./formkit.theme"
+
+const config = {
+  icons: { ...genesisIcons },
+  config: { rootClasses }
+}
+
+export default config
